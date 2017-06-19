8fc73860643ce1312585ce25cb065ac184ab9158
Michelle <michlee1337@gmail.com>
2017-06-19 17:40:29 +0800

    best first move

--- application.js
+++ application.js
@@ -1,36 +1,76 @@
-var board = [0,0,0,0,2,0,0,0,0]
+var board = [0,0,0,0,0,0,0,0,0]
 
 document.addEventListener('DOMContentLoaded', function() {
 
     $(".board").on('click', function() {
       //adapting the array
 
-      //Change the clicked tile to 1
+      //Change the clicked tile to show X
       var tile = this.id;
       if (tile === 0){
         return 0;
       }
       board[tile] = 1;
 
-      //changing a random empty tile to 2
+      for (var i=0; i<9; i++){
+        if (board[i] == 1) {
+          var tile = '#' + i;
+          $(tile).html("<p>X</p>");
+        }
+      }
+
+      //Making O's move
+
+      //if 1st X is corner
+      if ( $('.corner').children().length > 0 ) {
+        board[4] = 2;
+        console.log('corner');
+      };
+
+      //if 1st X is center !!randomise from 4 corners
+      if ( $('#4').children().length > 0 ) {
+        board[0] = 2;
+        console.log('center');
+      };
+
+      //if 1st X is edge
+      if ( $('.edge').children().length > 0 ) {
+        var eChange = this.id
+        var eRes = 0;
+        console.log(eChange);
+        if (eChange==1||eChange==7){
+          eRes = eChange - 1;
+          console.log('a');
+        }
+
+        if (eChange==3||eChange==5){
+          eRes = eChange - 3;
+          console.log('b');
+        }
+        board[eRes] = 2;
+        console.log('corner');
+      };
+
+      for (var i=0; i<9; i++){
+        if (board[i] == 2) {
+          var tile = '#' + i;
+          $(tile).html("<p>O</p>");
+        }
+      }
+
+
+
+/*      //changing a random empty tile to 2
       do {
           var count = Math.floor(Math.random() * 10);
       }
       while (board[count] !== 0)
 
       board[count] = 2;
+*/
 
       //Generate board
-      for (var i=0; i<9; i++){
-        if (board[i] == 1) {
-          var tile = '#' + i;
-          $(tile).html("<p>X</p>");
-        }
-        if (board[i] == 2) {
-          var tile = '#' + i;
-          $(tile).html("<p>O</p>");
-        }
-      }
+
 
     });
 
